46a7a57f583d827caaafa4058dbb1282303a37e5
Javier Bertos <javier.bertos@gmail.com>
2024-09-08 02:33:23 +0200

    Stage 3/5 Look around you

--- task/src/minesweeper/Main.kt
+++ task/src/minesweeper/Main.kt
@@ -21,8 +21,34 @@ class MinesWeeper (val rows: Int = 9, val columns: Int = 9, val mines: Int = 10)
         } while (this.minesLocation.size < this.mines)
     }
 
+    private fun calculateSurroundingMines(coords: List<Int>): String {
+        val rowIni = if (coords[0] == 0) 0 else coords[0] - 1
+        val rowEnd = if (coords[0] == this.rows - 1) this.rows - 1 else coords[0] + 1
+
+        val colIni = if (coords[1] == 0) 0 else coords[1] - 1
+        val colEnd = if (coords[1] == this.columns - 1) this.columns - 1 else coords[1] + 1
+
+        var minesCounter = 0
+
+        for (r in rowIni..rowEnd) {
+            for (c in colIni..colEnd) {
+                if (this.minesLocation.indexOf(listOf(r, c)) != -1) {
+                    minesCounter++
+                }
+            }
+        }
+
+        return if (minesCounter == 0) this.safeCell else minesCounter.toString()
+    }
+
     private fun getCellStatus(coords: List<Int>): String {
-        return if (this.minesLocation.indexOf(coords) == -1) this.safeCell else this.mineCell
+        val thereIsMineHere = this.minesLocation.indexOf(coords) != -1
+
+        if (thereIsMineHere) {
+            return this.mineCell
+        }
+
+        return this.calculateSurroundingMines(coords)
     }
 
     fun drawBoard() {
