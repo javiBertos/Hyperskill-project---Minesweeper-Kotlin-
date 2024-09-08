72243a9b0ac37e39ab2c5d2789e4a0ecef671a7d
Javier Bertos <javier.bertos@gmail.com>
2024-09-08 02:33:52 +0200

    Stage 4/5 Prepare for the battle

--- task/src/minesweeper/Main.kt
+++ task/src/minesweeper/Main.kt
@@ -1,11 +1,14 @@
 package minesweeper
 
+import java.util.Scanner
 import kotlin.random.Random
 
 class MinesWeeper (val rows: Int = 9, val columns: Int = 9, val mines: Int = 10) {
     private val mineCell = "X"
     private val safeCell = "."
+    private val markedCell = "*"
     private val minesLocation: MutableList<List<Int>> = mutableListOf()
+    private val cellsMarked: MutableList<List<Int>> = mutableListOf()
 
     init {
         this.setInitMinesOnBoard()
@@ -13,7 +16,7 @@ class MinesWeeper (val rows: Int = 9, val columns: Int = 9, val mines: Int = 10)
 
     private fun setInitMinesOnBoard() {
         do {
-            val newMine = listOf(Random.nextInt(rows - 1), Random.nextInt(columns - 1))
+            val newMine = listOf(Random.nextInt(1, rows), Random.nextInt(1, columns))
 
             if (this.minesLocation.indexOf(newMine) == -1) {
                 this.minesLocation.add(newMine)
@@ -21,18 +24,18 @@ class MinesWeeper (val rows: Int = 9, val columns: Int = 9, val mines: Int = 10)
         } while (this.minesLocation.size < this.mines)
     }
 
-    private fun calculateSurroundingMines(coords: List<Int>): String {
-        val rowIni = if (coords[0] == 0) 0 else coords[0] - 1
-        val rowEnd = if (coords[0] == this.rows - 1) this.rows - 1 else coords[0] + 1
+    private fun calculateSurroundingMines(cords: List<Int>): String {
+        val rowIni = if (cords[1] == 1) 1 else cords[1] - 1
+        val rowEnd = if (cords[1] == this.rows) this.rows else cords[1] + 1
 
-        val colIni = if (coords[1] == 0) 0 else coords[1] - 1
-        val colEnd = if (coords[1] == this.columns - 1) this.columns - 1 else coords[1] + 1
+        val colIni = if (cords[0] == 1) 1 else cords[0] - 1
+        val colEnd = if (cords[0] == this.columns) this.columns else cords[0] + 1
 
         var minesCounter = 0
 
         for (r in rowIni..rowEnd) {
             for (c in colIni..colEnd) {
-                if (this.minesLocation.indexOf(listOf(r, c)) != -1) {
+                if (this.minesLocation.indexOf(listOf(c, r)) != -1) {
                     minesCounter++
                 }
             }
@@ -41,32 +44,91 @@ class MinesWeeper (val rows: Int = 9, val columns: Int = 9, val mines: Int = 10)
         return if (minesCounter == 0) this.safeCell else minesCounter.toString()
     }
 
-    private fun getCellStatus(coords: List<Int>): String {
-        val thereIsMineHere = this.minesLocation.indexOf(coords) != -1
-
-        if (thereIsMineHere) {
-            return this.mineCell
+    private fun getCellStatus(cords: List<Int>): String {
+        if (this.minesLocation.indexOf(cords) != -1 || this.cellsMarked.indexOf(cords) != -1) {
+            return if (this.cellsMarked.indexOf(cords) != -1) this.markedCell else this.safeCell
         }
 
-        return this.calculateSurroundingMines(coords)
+        return this.calculateSurroundingMines(cords)
     }
 
     fun drawBoard() {
-        for (row in 0 until this.rows) {
-            for (col in 0 until this.columns) {
-                print(this.getCellStatus(listOf(row, col)))
+        for (row in 0..this.rows) {
+            for (col in 0..this.columns) {
+                if (col == 0) {
+                    print("${if (row == 0) " " else row}|")
+                    continue
+                }
+
+                if (row == 0) {
+                    print(col)
+                } else {
+                    print(this.getCellStatus(listOf(col, row)))
+                }
+
+                if (col == this.columns) {
+                    print("|")
+                }
             }
             println()
+
+            if (row == 0 || row == this.rows) {
+                println("-|${"-".repeat(this.columns)}|")
+            }
         }
+    }
+
+    private fun allAndOnlyMinesAreMarked(): Boolean {
+        var totalMatch = false
+
+        if (this.minesLocation.size == this.cellsMarked.size) {
+            return this.minesLocation.containsAll(this.cellsMarked)
+        }
+
+        return totalMatch
+    }
+
+    fun markCellAsMined(cords: List<Int>): Boolean {
+        if (this.minesLocation.indexOf(cords) == -1 && this.calculateSurroundingMines(cords) != this.safeCell) {
+            throw Exception("There is a number here!")
+        }
+
+        if (this.cellsMarked.indexOf(cords) != -1) {
+            this.cellsMarked.remove(cords)
+        } else {
+            this.cellsMarked.add(cords)
+        }
+
+        return this.allAndOnlyMinesAreMarked()
     }
 }
 
 fun main() {
+    // Init inputs reader
+    val reader = Scanner(System.`in`)
+
     println("How many mines do you want on the field?")
-    val userInputString = readln()
-    val userMinesInput = if (userInputString.toIntOrNull() != null) userInputString.toInt() else 10
+    val userMinesInput = reader.nextInt()
+    var gameFinished = false
 
     val minesWeeper = MinesWeeper(mines = userMinesInput)
 
     minesWeeper.drawBoard()
+
+    do {
+        println("Set/delete mines marks (x and y coordinates):")
+        val mineCol = reader.nextInt()
+        val mineRow = reader.nextInt()
+
+        try {
+            gameFinished = minesWeeper.markCellAsMined(listOf(mineCol, mineRow))
+
+            minesWeeper.drawBoard()
+        } catch (e: Exception) {
+            println(e.message)
+        }
+
+    } while (!gameFinished)
+
+    println("Congratulations! You found all the mines!")
 }
