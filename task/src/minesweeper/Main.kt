c68514f0d033d6cf187701491f10049a87af4687
Javier Bertos <javier.bertos@gmail.com>
2024-09-08 02:32:49 +0200

    Stage 2/5 Flexible mines

--- task/src/minesweeper/Main.kt
+++ task/src/minesweeper/Main.kt
@@ -2,7 +2,7 @@ package minesweeper
 
 import kotlin.random.Random
 
-class MinesWeeper (val rows: Int = 10, val columns: Int = 10, val mines: Int = 10) {
+class MinesWeeper (val rows: Int = 9, val columns: Int = 9, val mines: Int = 10) {
     private val mineCell = "X"
     private val safeCell = "."
     private val minesLocation: MutableList<List<Int>> = mutableListOf()
@@ -18,7 +18,7 @@ class MinesWeeper (val rows: Int = 10, val columns: Int = 10, val mines: Int = 1
             if (this.minesLocation.indexOf(newMine) == -1) {
                 this.minesLocation.add(newMine)
             }
-        } while (this.minesLocation.size < this.mines - 1)
+        } while (this.minesLocation.size < this.mines)
     }
 
     private fun getCellStatus(coords: List<Int>): String {
@@ -36,7 +36,11 @@ class MinesWeeper (val rows: Int = 10, val columns: Int = 10, val mines: Int = 1
 }
 
 fun main() {
-    val minesWeeper = MinesWeeper()
+    println("How many mines do you want on the field?")
+    val userInputString = readln()
+    val userMinesInput = if (userInputString.toIntOrNull() != null) userInputString.toInt() else 10
+
+    val minesWeeper = MinesWeeper(mines = userMinesInput)
 
     minesWeeper.drawBoard()
 }
